2d5ad7d396891f8fdf3a0127c16f0c99ab7ab070
RupalKeshwala29 <keshwala.rupal@gmail.com>
2019-02-05 11:37:11 +0300

    Second commit DJ laptop

---
+++ app/src/main/java/com/example/notificationapp/AdminLandingPage.java
@@ -0,0 +1,60 @@
+package com.example.notificationapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AdminLandingPage extends AppCompatActivity {
+    private Button AddUser,ModifyUser,ModifyGroup,ModifyEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_landing_page);
+
+        AddUser= (Button) findViewById(R.id.AddUser);
+        ModifyUser=(Button) findViewById(R.id.ModifyUser);
+        ModifyEvent=(Button) findViewById(R.id.ModifyEvent);
+        ModifyGroup=(Button) findViewById(R.id.ModifyGroup);
+
+        AddUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),AddUserActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
+
+        ModifyUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ModifyUserActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
+
+        ModifyEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),EventActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
+
+        ModifyGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),GroupActivity.class);
+                startActivity(intent);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            }
+        });
+    }
+
+}
